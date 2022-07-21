7661c2c6eb7718f62cf9833ec5a29149270c18a7
Pierre Paquin <p.paquin1016@laposte.net>
2022-07-21 21:56:30 +0200

    feat: ajout icon de test + changer le nom

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 function love.conf(t)
     t.window.width = 600
     t.window.height = 600
-    t.window.title = "dice-dungeon"
+    t.window.title = "lua-love-seed -- @paq1"
+    t.window.icon = "assets/icons/icon-test.png"
     t.console = true
 end
